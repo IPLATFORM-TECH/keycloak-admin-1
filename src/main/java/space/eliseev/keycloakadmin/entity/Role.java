ad7073b510e66b66ad679c7a57202f5b7c854809
VahitovRashid <105593724+VahitovRashid@users.noreply.github.com>
2022-07-14 18:24:53 +0300

    KA-4 add KeycloakRole entity (#1)
    * KA-4 add Role entity

---
+++ src/main/java/space/eliseev/keycloakadmin/entity/Role.java
@@ -0,0 +1,36 @@
+package space.eliseev.keycloakadmin.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "keycloak_role")
+public class Role extends BaseEntity {
+
+    @Column(name = "client_realm_constraint")
+    private String clientRealmConstraint;
+
+    @Column(name = "client_role")
+    private Boolean clientRole;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "realm_id")
+    private String realmId;
+
+    @Column(name = "client")
+    private String client;
+
+    @Column(name = "realm")
+    private String realm;
+}

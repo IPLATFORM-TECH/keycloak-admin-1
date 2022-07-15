3bce010ede806cd980149125d076b27416d5d995
VasilisaLukishina <vasa.luk.2001@mail.ru>
2022-07-15 03:55:44 +0300

    KA-11 сервис получения информации для client

--- src/main/java/space/eliseev/keycloakadmin/controller/ClientController.java
+++ src/main/java/space/eliseev/keycloakadmin/controller/ClientController.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 public class ClientController {
     private final ClientService clientService;
 
-    @GetMapping(value = "/getAll")
+    @GetMapping("/getAll")
     public ResponseEntity<List<Client>> getClients() {
         return new ResponseEntity<>(clientService.getAllClients(), HttpStatus.OK);
     }

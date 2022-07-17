29b7d354525a3a521ae5bc52b804d30af102713a
Vasaluk <101427439+Vasaluk@users.noreply.github.com>
2022-07-17 22:43:07 +0300

    Ka 11 (#8)
    * KA-11 сервис получения информации для client

---
+++ src/main/java/space/eliseev/keycloakadmin/service/ClientService.java
@@ -0,0 +1,27 @@
+package space.eliseev.keycloakadmin.service;
+
+import lombok.NonNull;
+import space.eliseev.keycloakadmin.entity.Client;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClientService {
+    List<Client> getAllClients();
+    Optional<Client> getById(@NonNull String id);
+    Optional<Client> getByName(@NonNull String name);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+

72b672ffbeb59b2d2925dd2b6b3b3721d524a34b
naturemistake666 <65819256+naturemistake666@users.noreply.github.com>
2022-07-24 18:03:37 +0300

    add Test class for TimeUtils

--- src/main/java/space/eliseev/keycloakadmin/test/TimeUtilsTest.java
+++ src/main/java/space/eliseev/keycloakadmin/test/TimeUtilsTest.java
@@ -1,16 +1,23 @@
-package space.eliseev.keycloakadmin.commons;
+package space.eliseev.keycloakadmin.test;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import space.eliseev.keycloakadmin.commons.TimeUtils;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TimeUtilsTest {
 
+    @BeforeAll
+    public static void startMethod() {
+        System.out.println("to local data time");
+        System.out.println("to long");
+    }
+
     @Test
     public void testToLocalDateTime() {
-        System.out.println("to local data time");
         long time = 1499070360;
         LocalDateTime expResult = LocalDateTime.parse("2017-07-03T11:26");
         LocalDateTime result = TimeUtils.toLocalDateTime(time);
@@ -19,7 +26,6 @@ class TimeUtilsTest {
 
     @Test
     public void testToLong() {
-        System.out.println("to long");
         LocalDateTime time = LocalDateTime.parse("2017-07-03T11:26");
         long expResult = 1499070360L;
         long result = TimeUtils.toLong(time);

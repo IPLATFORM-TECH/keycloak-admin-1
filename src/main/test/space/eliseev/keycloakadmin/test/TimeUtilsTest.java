7bf887a26c6e678cde84544bdac4df4858b818ec
naturemistake666 <65819256+naturemistake666@users.noreply.github.com>
2022-07-28 10:58:05 +0300

    create utility class TimeUtils (#16)
    * create utility class TimeUtils
    
    * create utility class TimeUtils
    
    * create utility class TimeUtils
    
    * create utility class TimeUtils
    
    * add Test class for TimeUtils
    
    * add Test class for TimeUtils
    
    * add Test class for TimeUtils

---
+++ src/main/test/space/eliseev/keycloakadmin/test/TimeUtilsTest.java
@@ -0,0 +1,44 @@
+package space.eliseev.keycloakadmin.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import space.eliseev.keycloakadmin.commons.TimeUtils;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class TimeUtilsTest {
+    private static long time;
+    private static long time2;
+    private static LocalDateTime localDateTime;
+    private static LocalDateTime localDateTime2;
+
+
+    @BeforeAll
+    static void startMethod() {
+        time = 1499070360;
+        time2 = 1593769560;
+        localDateTime = LocalDateTime.parse("2017-07-03T11:26");
+        localDateTime2 = LocalDateTime.parse("2020-07-03T12:46");
+    }
+
+    @Test
+    void testToLocalDateTime() {
+
+        Assertions.assertEquals(TimeUtils.toLocalDateTime(time), localDateTime);
+        assertNotEquals(TimeUtils.toLocalDateTime(time), localDateTime2);
+        assertEquals(TimeUtils.toLocalDateTime(time2), localDateTime2);
+        assertNotEquals(TimeUtils.toLocalDateTime(time2), localDateTime);
+    }
+
+    @Test
+    void testToLong() {
+        assertEquals(TimeUtils.toLong(localDateTime), time);
+        assertEquals(TimeUtils.toLong(localDateTime2), time2);
+        assertNotEquals(TimeUtils.toLong(localDateTime), time2);
+        assertNotEquals(TimeUtils.toLong(localDateTime2), time);
+    }
+}

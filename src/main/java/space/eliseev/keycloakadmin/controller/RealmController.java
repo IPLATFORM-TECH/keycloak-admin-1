fb56b97be4d13bf8ce338fd1cf66178e97a824c8
GolDiBorh <reznichenko.sania@yandex.ru>
2022-07-28 13:47:22 +0300

    Реализация realmDTO

--- src/main/java/space/eliseev/keycloakadmin/controller/RealmController.java
+++ src/main/java/space/eliseev/keycloakadmin/controller/RealmController.java
@@ -1,5 +1,7 @@
 package space.eliseev.keycloakadmin.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,16 +19,19 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value ="/realm", produces = "application/json; charset=UTF-8")
+@Tag(name = "Realm", description = "The Realm API")
 public class RealmController {
 
     private final RealmService realmService;
 
     @GetMapping(value = "/getAll")
+    @Operation(summary = "Gets all realms")
     public ResponseEntity<List<RealmDto>> getRealms() {
         return new ResponseEntity<>(realmService.getAllRealms(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/getById/{id}")
+    @Operation(summary = "Gets realm by ID")
     public ResponseEntity<RealmDto> getById(@RequestParam String id) {
 
         final Optional<RealmDto> realm = realmService.getById(id);
@@ -36,6 +41,7 @@ public class RealmController {
     }
 
     @GetMapping(value = "/getByName/{name}")
+    @Operation(summary = "Gets realm by Name")
     public ResponseEntity<RealmDto> getByName (@RequestParam String name) {
 
         final Optional<RealmDto> realm = realmService.getByName(name);

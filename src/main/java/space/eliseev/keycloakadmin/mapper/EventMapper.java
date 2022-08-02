24cbec8af1de2112de0e9252e3a9e9c1d32cf0e2
Aar1s <65885326+Aar1s@users.noreply.github.com>
2022-08-02 10:54:15 +0500

    KA-58, исправил TimeUtils (сделать всё в миллисекундах) и добавил маппер для EventDto.

--- src/main/java/space/eliseev/keycloakadmin/mapper/EventMapper.java
+++ src/main/java/space/eliseev/keycloakadmin/mapper/EventMapper.java
@@ -1,12 +1,16 @@
 package space.eliseev.keycloakadmin.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import space.eliseev.keycloakadmin.commons.TimeUtils;
 import space.eliseev.keycloakadmin.dto.EventDto;
 import space.eliseev.keycloakadmin.entity.Event;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", imports = TimeUtils.class)
 public interface EventMapper {
+    @Mapping(target = "eventTime", expression = "java(TimeUtils.toLocalDateTime(event.getEventTime()))")
     EventDto toDto(Event event);
 
+    @Mapping(target = "eventTime", expression = "java(TimeUtils.toLong(eventDto.getEventTime()))")
     Event toEntity(EventDto eventDto);
 }

a1acf0337e4d035f4a67e10c8e6f309a2a2f248c
Vasaluk <101427439+Vasaluk@users.noreply.github.com>
2022-08-05 10:17:01 +0300

    KA-56 Сервис ПФ для Client (#37)

--- src/main/java/space/eliseev/keycloakadmin/service/FormBuilderService.java
+++ src/main/java/space/eliseev/keycloakadmin/service/FormBuilderService.java
@@ -2,7 +2,6 @@ package space.eliseev.keycloakadmin.service;
 
 import lombok.NonNull;
 import space.eliseev.keycloakadmin.commons.TypeFile;
-import space.eliseev.keycloakadmin.entity.Client;
 import space.eliseev.keycloakadmin.entity.Realm;
 import space.eliseev.keycloakadmin.entity.Role;
 
@@ -10,7 +9,7 @@ import java.util.List;
 
 public interface FormBuilderService {
 
-    List<Client> downloadAllClients();
+    byte[] downloadAllClients(@NonNull TypeFile typeFile);
 
     List<Realm> downloadAllRealm();
 
--- src/main/java/space/eliseev/keycloakadmin/service/FormBuilderServiceImpl.java
+++ src/main/java/space/eliseev/keycloakadmin/service/FormBuilderServiceImpl.java
@@ -4,9 +4,9 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import space.eliseev.keycloakadmin.commons.TypeFile;
-import space.eliseev.keycloakadmin.entity.Client;
 import space.eliseev.keycloakadmin.entity.Realm;
 import space.eliseev.keycloakadmin.entity.Role;
+import space.eliseev.keycloakadmin.service.factory.ClientFormBuilderFactory;
 import space.eliseev.keycloakadmin.service.factory.UserFormBuilderFactory;
 
 import java.util.List;
@@ -16,10 +16,12 @@ import java.util.List;
 public class FormBuilderServiceImpl implements FormBuilderService {
 
     private final UserFormBuilderFactory userFormBuilderFactory;
+    private final ClientFormBuilderFactory clientFormBuilderFactory;
     private final UserService userService;
+    private final ClientService clientService;
 
-    public List<Client> downloadAllClients() {
-        return null;
+    public byte[] downloadAllClients(@NonNull TypeFile typeFile) {
+        return clientFormBuilderFactory.download(clientService.getAllClients(), typeFile);
     }
 
     public List<Realm> downloadAllRealm() {

6cb57323f912f0af45f9e16c70a804d9f629385a
Vasaluk <101427439+Vasaluk@users.noreply.github.com>
2022-08-11 11:22:53 +0300

    KA-7 спецификация ClientController (#46)

--- src/main/java/space/eliseev/keycloakadmin/controller/ClientController.java
+++ src/main/java/space/eliseev/keycloakadmin/controller/ClientController.java
@@ -2,6 +2,11 @@ package space.eliseev.keycloakadmin.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,7 +16,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import space.eliseev.keycloakadmin.dto.ClientDto;
-import space.eliseev.keycloakadmin.entity.Client;
 import space.eliseev.keycloakadmin.service.ClientService;
 
 import java.util.List;
@@ -20,19 +24,43 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value ="/client", produces = "application/json; charset=UTF-8")
-@Tag(name = "Client", description = "The Client API")
+@Tag(name = "Client API", description = "Получение информации о клиентах")
 public class ClientController {
     private final ClientService clientService;
 
-    @Operation(summary = "Gets all clients")
+    @Operation(summary = "Get all clients",
+            description = "Получить список всех клиентов",
+            tags = {"Client API"})
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = ClientDto.class))),
+                    description = "Successful operation")
+    })
     @GetMapping("/getAll")
     public ResponseEntity<List<ClientDto>> getClients() {
         return new ResponseEntity<>(clientService.getAllClients(), HttpStatus.OK);
     }
 
-    @Operation(summary = "Gets client by id")
+    @Operation(
+            summary = "Get client by id",
+            description = "Получить пользователя по идентификатору",
+            tags = {"Client API"})
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = ClientDto.class))),
+                    description = "Successful operation"),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content,
+                    description = "Client not found")
+    })
     @GetMapping("/getById/{id}")
-
     public ResponseEntity<ClientDto> getById(@PathVariable @Parameter(description = "client id") String id) {
 
         final Optional<ClientDto> client = clientService.getById(id);
@@ -42,9 +70,23 @@ public class ClientController {
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @Operation(summary = "Gets client by name")
+    @Operation(
+            summary = "Get client by name",
+            description = "Получить пользователя по идентификатору",
+            tags = {"Client API"})
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = ClientDto.class))),
+                    description = "Successful operation"),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content,
+                    description = "Client not found")
+    })
     @GetMapping("/getByName/{name}")
-
     public ResponseEntity<ClientDto> getByName(@PathVariable @Parameter(description = "client name") String name) {
 
         final Optional<ClientDto> client = clientService.getByName(name);

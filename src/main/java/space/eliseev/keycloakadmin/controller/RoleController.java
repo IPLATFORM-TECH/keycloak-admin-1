598747100451470cc9f84949285afdc1fd4b50af
fijirash <98469366+fijirash@users.noreply.github.com>
2022-08-15 09:37:37 +0300

    add OpenAPI Role (#54)

--- src/main/java/space/eliseev/keycloakadmin/controller/RoleController.java
+++ src/main/java/space/eliseev/keycloakadmin/controller/RoleController.java
@@ -2,6 +2,11 @@ package space.eliseev.keycloakadmin.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -27,14 +32,48 @@ public class RoleController {
 
     private final RoleService roleService;
 
+    @Operation(
+            summary = "Get all roles",
+            description = "Получить список всех ролей",
+            tags = {"Role API"}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = RoleDto.class))
+
+                    ),
+                    description = "Successful operation"
+            )
+    })
     @GetMapping(value = "/getAll")
-    @Operation(summary = "Получение всех ролей")
     public ResponseEntity<List<RoleDto>> getAll() {
         return new ResponseEntity<>(roleService.getAllRoles(), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Get role by ID",
+            description = "Получить роль по идентификатору",
+            tags = {"Role API"}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = RoleDto.class))
+                    ),
+                    description = "Successful operation"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content,
+                    description = "Role not found"
+            )
+    })
     @GetMapping("/getById/{id}")
-    @Operation(summary = "Получение роли по ID")
     public ResponseEntity<RoleDto> getById(@Parameter(description = "ID роли, которую нужно показать")
                                            @PathVariable String id) {
 
@@ -45,8 +84,27 @@ public class RoleController {
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(
+            summary = "Get role by name",
+            description = "Получить роль по названию роли",
+            tags = {"Role API"}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = RoleDto.class))
+                    ),
+                    description = "Successful operation"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content,
+                    description = "Role not found"
+            )
+    })
     @GetMapping("/getByName/{name}")
-    @Operation(summary = "Получение роли по названию")
     public ResponseEntity<RoleDto> getByName(@Parameter(description = "Название роли, которую нужно показать")
                                              @PathVariable String name) {
 

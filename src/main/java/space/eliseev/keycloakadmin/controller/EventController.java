a955411c5b48e6b0e1bcbcd917bbe1cca1909d82
Mark <60811858+MrK103@users.noreply.github.com>
2022-08-15 09:39:51 +0300

    K-8 OpenAPI спецификация EventController (#49)

--- src/main/java/space/eliseev/keycloakadmin/controller/EventController.java
+++ src/main/java/space/eliseev/keycloakadmin/controller/EventController.java
@@ -1,5 +1,13 @@
 package space.eliseev.keycloakadmin.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,17 +25,51 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/event", produces = "application/json; charset=UTF-8")
+@Tag(name = "Event API", description = "Получение информации о событиях")
 public class EventController {
 
     private final EventService eventService;
 
+    @Operation(
+            summary = "Get all events",
+            description = "Получить список всех событий",
+            tags = {"Event API"})
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = EventDto.class))),
+                    description = "Successful operation")
+    })
     @GetMapping(value = "/getAll")
     public ResponseEntity<List<EventDto>> getEvents() {
         return new ResponseEntity<>(eventService.getAllEvents(), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Get event by ID",
+            description = "Получить событие по идентификатору",
+            tags = {"Event API"})
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = EventDto.class))),
+                    description = "Successful operation"),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content,
+                    description = "Event not found")
+    })
     @GetMapping(value = "/getById/id={id}")
-    public ResponseEntity<EventDto> getById(@PathVariable String id) {
+    public ResponseEntity<EventDto> getById(
+            @Parameter(
+                    required = true,
+                    description = "Идентификатор событий")
+            @PathVariable String id) {
+
         final Optional<EventDto> event = eventService.getById(id);
 
         return event
@@ -35,6 +77,7 @@ public class EventController {
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(summary = "Gets event by username")
     @GetMapping(value = "/getByUsername/username={username}")
     public ResponseEntity<EventDto> getByUsername(@PathVariable String username) {
         final Optional<EventDto> event = eventService.getByUsername(username);
@@ -44,12 +87,14 @@ public class EventController {
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(summary = "Gets event by date")
     @GetMapping(value = "/getByDate/startDate={localDateTimeStart:-*}&endDate={localDateTimeEnd:-*}")
     public ResponseEntity<List<EventDto>> getByDate(@PathVariable LocalDateTime localDateTimeStart, @PathVariable LocalDateTime localDateTimeEnd) {
         final List<EventDto> event = eventService.getByDate(localDateTimeStart, localDateTimeEnd);
         return new ResponseEntity<>(event, HttpStatus.OK);
     }
 
+    @Operation(summary = "Gets event by username and date")
     @GetMapping(value = "/getAllBy/username={username}&startDate={localDateTimeStart:_*}&endDate={localDateTimeEnd:_*}")
     public ResponseEntity<List<EventDto>> getByUsernameAndDate(@PathVariable String username, @PathVariable LocalDateTime localDateTimeStart, @PathVariable LocalDateTime localDateTimeEnd) {
         final List<EventDto> event = eventService.getByUsernameAndDte(username, localDateTimeStart, localDateTimeEnd);
